367a8d0f1128c2214e51548150ef25c76e65cb60
Chimbori <hello@chimbori.com>
2024-08-30 11:41:31 -0700

    Save files extracted from the HAR file to the local directory

--- save/save.go
+++ save/save.go
@@ -1,9 +1,55 @@
 package save
 
 import (
+	"encoding/base64"
 	"log"
+	"net/url"
+	"os"
+	"path"
+
+	"go.chimbori.app/harvest"
 )
 
 func Save(args []string) {
 	log.Printf("Save")
+
+	for _, fileName := range args {
+		har, err := harvest.Parse(fileName)
+		if err != nil {
+			log.Println(err)
+		}
+
+		for _, entry := range har.Log.Entries {
+			// log.Println(entry.Response.Content.MimeType)
+			// log.Println(entry.Request.URL)
+			// log.Println(entry.Response.Content.Text)
+
+			var content []byte
+			if entry.Response.Content.Encoding == "base64" {
+				content, _ = base64.StdEncoding.DecodeString(entry.Response.Content.Text)
+			} else {
+				content = []byte(entry.Response.Content.Text)
+			}
+
+			fileName, err := getFileName(entry.Request.URL)
+			if err != nil {
+				log.Println(err)
+			}
+
+			log.Println(entry.Request.URL, " --> ", fileName)
+			err = os.WriteFile("tmp-"+fileName, content, 0o644)
+			if err != nil {
+				log.Println(err)
+			}
+		}
+	}
+}
+
+func getFileName(s string) (fileName string, err error) {
+	u, err := url.Parse(s)
+	if err != nil {
+		panic(err)
+	}
+	fileName = path.Base(u.Path)
+	return
 }
